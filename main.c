82788a313999cd787674511432557ca1e9496664
Talrem <43709968+Talrem@users.noreply.github.com>
2019-03-01 10:09:57 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,31 @@
 #include <time.h>
 #include "./Aff/aff.h"
 #include "./Init/init.h"
+#include "./Tour/tour.h"
 
-int main() {
-   system("clear");
-   char * plat[TAILLE][TAILLE];
-   srand(time(NULL));
-   initPlateau(plat);
-   initTestRandPlat(plat);
-   afficherPlateau(plat);
-   afficheScore(plat);
-   return 0;
+int estFinie(char * plat[TAILLE][TAILLE]){
+  int done = 0;
+  return done;
+}
+
+int coupPossible(char * plat[TAILLE][TAILLE],char * tour){
+  int possible = 1;
+  return possible;
+}
+
+int main(int argc, char * argv[]) {
+  system("clear");
+  char * plat[TAILLE][TAILLE];
+  srand(time(NULL));
+  initPlateau(plat);
+  //initTestRandPlat(plat);
+  char * tour = NOIR;
+  while(!estFinie(plat)){
+    afficherPlateau(plat);
+    if(coupPossible(plat,tour)) saisieCoup(plat,tour);
+    tour = (tour == NOIR ? BLANC : NOIR);
+  }
+  afficherPlateau(plat);
+  afficheScore(plat);
+  return 0;
 }
--- makefile
+++ makefile
@@ -1,12 +1,13 @@
-out : main.o aff.o init.o
-	gcc main.o aff.o init.o -o out
+out : main.o aff.o init.o tour.o
+	gcc main.o aff.o init.o tour.o -o out
 main.o : main.c
 	gcc main.c -c
 aff.o : ./Aff/aff.c
 	gcc ./Aff/aff.c -c
 init.o : ./Init/init.c
 	gcc ./Init/init.c -c
-
+tour.o : ./Tour/tour.c
+	gcc ./Tour/tour.c -c
 
 clean:
 	rm *.o
