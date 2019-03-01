01403df04582ed36836c21d608e232503200c403
Talrem <43709968+Talrem@users.noreply.github.com>
2019-03-01 22:53:04 +0100

    Add files via upload

--- Aff/aff.c
+++ Aff/aff.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define TAILLE 8
-#define NOIR "●"
-#define BLANC "○"
+#include "../Aff/aff.h"
 
 void afficherPlateau(char * plat[TAILLE][TAILLE]) {
    int i, j;
--- Aff/aff.h
+++ Aff/aff.h
@@ -1,6 +1,6 @@
 #define TAILLE 8
-#define NOIR "●"
-#define BLANC "○"
+#define NOIR "0"
+#define BLANC "1"
 
 void afficherPlateau(char * plat[TAILLE][TAILLE]);
 void afficheScore(char * plat[TAILLE][TAILLE]);
