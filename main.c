53e8a0de63ead8d8433514b5e5c3cb4e64438b89
Talrem <43709968+Talrem@users.noreply.github.com>
2019-03-01 22:50:49 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "./Aff/aff.h"
 #include "./Init/init.h"
 #include "./Tour/tour.h"
 
 int estFinie(char * plat[TAILLE][TAILLE]){
   int done = 0;
+  int plein = 1;
+  int noirPresent = 0;
+  int blancPresent = 0;
+  int ligne,colonne;
+  for(ligne = 0; ligne < TAILLE; ligne++){
+    for(colonne = 0; colonne < TAILLE; colonne++){
+      //strcmp renvoit 0 en cas d'égalité des deux termes. Donc on cherche à savoir si
+      //Il y a encore au moins une case vide
+      if(!(strcmp(plat[ligne][colonne], " "))) plein = 0;
+      //Il y a encore au moins une case noire
+      if(!(strcmp(plat[ligne][colonne], NOIR))) noirPresent = 1;
+      //Il y a encore au moins une case blanche
+      if(!(strcmp(plat[ligne][colonne], BLANC))) blancPresent = 1;
+    }
+  }
+  if(plein || !noirPresent || !blancPresent) done = 1;
   return done;
 }
 
 int coupPossible(char * plat[TAILLE][TAILLE],char * tour){
   int possible = 1;
+  /*
+  A FAIRE
+  */
   return possible;
 }
 
@@ -28,8 +48,7 @@ int main(int argc, char * argv[]) {
     printf(" 5 - Joueur contre IA difficile\n");
     printf(" 6 - Quitter\n");
     printf("A quoi voulez vous jouer ? : ");
-    scanf("%d",&choixMenu);
-    getchar();
+    scanf("%i",&choixMenu);
   }
   choixMenu--;
   if(choixMenu == 5){
@@ -38,8 +57,13 @@ int main(int argc, char * argv[]) {
   }
   void (*tabFonc[5])(char * (*)[],char *) = {saisieCoupJcJL,saisieCoupJcJD,saisieCoupJcIF,saisieCoupJcII,saisieCoupJcID};
   void (*fonc)(char * (*)[],char *) = tabFonc[choixMenu];
+  initTestVide(plat);
   initPlateau(plat);
   //initTestRandPlat(plat);
+  //initTestPlein(plat);
+  //initTestPasDeBlanc(plat);
+  //initTestPasDeNoir(plat);
+  printf("Noir : %s / Blanc : %s",NOIR,BLANC);
   char * tour = NOIR;
   while(!estFinie(plat)){
     afficherPlateau(plat);
@@ -48,5 +72,6 @@ int main(int argc, char * argv[]) {
   }
   afficherPlateau(plat);
   afficheScore(plat);
+  system("PAUSE");
   return 0;
 }
