74ca54d09d8880401f285dc8158db53c08914032
Talrem <43709968+Talrem@users.noreply.github.com>
2019-03-05 16:39:25 +0100

    Add files via upload

--- Tour/tour.c
+++ Tour/tour.c
@@ -4,6 +4,83 @@
 #include "../definitions.h"
 
 /*
+Cette fonction renvoit si des pions vont être pris.
+Elle parcourt le terrain dans chaque direction en partant du point et met les couleurs qu'elle rencontre
+-1 pour les " ", 0 pour la couleur adverse et 1 pour la couleur actuelle, dans un tableau
+Puis après chaque direction, elle regarde dans le tableau si on a une suite de 0 puis un 1 et si c'est le cas
+elle retourne 1 car ça veut dire qu'un pion peut être pris. Sinon elle répète l'opération avec les autres directions
+et renvoit 0 sinon*/
+int prendPion(char * plat[TAILLE][TAILLE],int ligne, int colonne,char * couleur){
+  int aPris = 0;
+  int aPoint = 0;
+  int aAutreCouleur = 0;
+  int points[TAILLE - 1]={-1,-1,-1,-1,-1,-1,-1};
+  int i;
+  int j;
+  /*vers le haut*/
+  if(ligne != 0)
+    for(i = ligne - 1; i > 0;i--){
+      j = colonne;
+      if(!strcmp(plat[i][j]," "){
+        points[i] = -1;
+      }else if(strcmp(plat[i][j],couleur){
+        points[i] = 0;
+      }
+      printf("\nligne : %d\ncolonne :%d\ncase : '%s'",i,j,couleur);
+    }
+  /*vers le bas*/
+  if(ligne + 1 != TAILLE)
+    for(i = ligne + 1; i < TAILLE;i++){
+      j = colonne;
+      if(!strcmp(plat[i][j]," "){
+        points[i] = -1;
+      }else if(strcmp(plat[i][j],couleur){
+        points[i] = 0;
+      }
+      printf("\nligne : %d\ncolonne :%d\ncase : '%s'",i,j,couleur);
+    }
+  /*vers la gauche*/
+  if(colonne != 0)
+    for(j = colonne ; j > 0 ; j--){
+      i = ligne;
+      if(!strcmp(plat[i][j]," "){
+        points[i] = -1;
+      }else if(strcmp(plat[i][j],couleur){
+        points[i] = 0;
+      }
+      printf("\nligne : %d\ncolonne :%d\ncase : '%s'",i,j,couleur);
+    }
+  /*vers la droite*/
+  if(colonne + 1 != TAILLE)
+  for(j = colonne ; j < TAILLE;j++){
+    i = ligne;
+    if(!strcmp(plat[i][j]," "){
+      points[i] = -1;
+    }else if(strcmp(plat[i][j],couleur){
+      points[i] = 0;
+    }
+    printf("\nligne : %d\ncolonne :%d\ncase : '%s'",i,j,couleur);
+  }
+  /*diagonale HG*/
+  /*for(){
+
+  }*/
+  /*diagonale HD*/
+  /*for(){
+
+  }*/
+  /*diagonale BD*/
+  /*for(){
+
+  }*/
+  /*diagonale BG*/
+  /*for(){
+
+  }*/
+  return aPris;
+}
+
+/*
 Cette fonction vérifie la validité du coup
 Applique les changements au plateau
 Renvoit si oui ou non il y a eu une modification
@@ -21,9 +98,10 @@ int estValide(char * plat[TAILLE][TAILLE],int ligne, int colonne,char * couleur)
     return 0;
   }
   //coup prend un pion adverse.
-  /*
-  A FAIRE
-  */
+  if(!prendPion(plat,ligne,colonne,couleur)){
+    printf("\nLe coup que vous avez voulu jouer ne prend pas de pion adverse...\n");
+    return 0;
+  }
   plat[ligne][colonne] = couleur;
   return 1;
 }
