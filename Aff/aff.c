c9f38b53c5403f2dceff5f2bb475f01c62eae198
Talrem <43709968+Talrem@users.noreply.github.com>
2019-03-08 23:29:38 +0100

    Add files via upload

--- Aff/aff.c
+++ Aff/aff.c
@@ -4,23 +4,31 @@
 #include "../Aff/aff.h"
 
 void afficherPlateau(char * plat[TAILLE][TAILLE]) {
-   int i, j;
-   printf("\n |");
-   for(j = 0; j < TAILLE; j++){
-      printf("%d ", j + 1);
-   }
-   printf("\n_|");
-   for(j = 0; j < TAILLE; j++){
-      printf("__");
-   }
-   printf("\n");
-   for(i = 0; i < TAILLE; i++){
-      printf("%c|", 'A' + i);
-      for(j = 0; j < TAILLE; j++){
-         printf("%s ", plat[i][j]);
-      }
-      printf("\n");
-   }
+    int i, j;
+    char a = 'A';
+    printf("\n");
+    for(i = 1;i<=TAILLE+1;i++){
+      printf("o~~~");
+    }
+    /* Affichage des absices */
+    printf ("o\n|   |");
+    for (i=1; i<=TAILLE; i++) {
+        printf (" %d |", i);
+    }
+    /* Affichage de la grille */
+    printf ("\no");
+    for (i=0; i<TAILLE+1; i++)
+        printf ("~~~o");
+    printf ("\n");
+    for (i=0; i<TAILLE; i++) {
+        printf ("| %c ", a++);
+        for (j=0; j<TAILLE; j++)
+                printf ("| %s ", plat[i][j]);
+        printf ("|\no");
+        for (j=0; j<=TAILLE; j++)
+            printf ("~~~o");
+        printf ("\n");
+    }
 }
 
 void afficheScore(char * plat[TAILLE][TAILLE]){
