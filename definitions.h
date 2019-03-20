bbffefae7b6bd337e15d98138d447b057061fc6e
Talrem <43709968+Talrem@users.noreply.github.com>
2019-03-20 14:00:10 +0100

    Add files via upload

--- definitions.h
+++ definitions.h
@@ -1,4 +1,11 @@
 #define TAILLE 8
 #define VIDE " "
+
+#define NOIR "●"
+#define BLANC "○"
+
+
+/*
 #define NOIR "@"
 #define BLANC "O"
+*/
--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "./Aff/aff.h"
 #include "./Init/init.h"
 #include "./Tour/tour.h"
+#include "./Reseau/reseau.h"
 #include "./definitions.h"
 
 /*
@@ -55,6 +56,7 @@ int main(int argc, char * argv[]){
   int choixMenu = -1;
   int choixMenuIA = -1;
   int choixMenuJ = -1;
+  int choixMenuJD = -1;
   /*Par défaut, la difficulte est sur facile*/
   int difficulte = 1;
   /*Si la couleur de l'IA est 1 alors elle jouera les noirs, sinon elle jouera les blancs*/
@@ -70,13 +72,13 @@ int main(int argc, char * argv[]){
     scanf("%i",&choixMenu);
     getchar();
   }
-  system("clear");
   if(choixMenu == 3){
     printf("\nAu revoir !\n\n");
     return 0;
   }
   if(choixMenu == 1){
-    while(choixMenuJ <= 0 || choixMenu > 3){
+    while(choixMenuJ < 1 || choixMenuJ > 3){
+      system("clear");
       printf(" 1 - Joueur Local\n");
       printf(" 2 - Joueur Distant\n");
       printf(" 3 - Quitter\n");
@@ -87,27 +89,43 @@ int main(int argc, char * argv[]){
     if(choixMenuJ == 3){
       printf("\nAu revoir !\n\n");
       return 0;
+    }else if(choixMenuJ == 2){
+      while(choixMenuJD < 1 || choixMenuJD > 3){
+        system("clear");
+        printf(" 1 - Héberger une partie\n");
+        printf(" 2 - Rejoindre une partie\n");
+        printf(" 3 - Quitter\n");
+        printf("Que choisissez vous ? : ");
+        scanf("%i",&choixMenuJD);
+        getchar();
+      }
+      /*
+      C'est là qu'il faut mettre le code pour le réseau
+      */
     }
   }else{
-    while(choixMenuIA <= 0 || choixMenuIA > 3){
+    while(choixMenuIA < 1 || choixMenuIA > 5){
+      system("clear");
       printf(" 1 - IA facile\n");
       printf(" 2 - IA intermédiaire\n");
       printf(" 3 - IA difficile\n");
-      printf(" 4 - Quitter\n");
+      printf(" 4 - IA expert\n");
+      printf(" 5 - Quitter\n");
       printf("Quel mode de jeu choisissez vous ? : ");
       scanf("%i",&choixMenuIA);
       getchar();
     }
-    if(choixMenuIA == 4){
+    if(choixMenuIA == 5){
       printf("\nAu revoir !\n\n");
       return 0;
     }
   }
   if(choixMenuIA != -1){
     switch(choixMenuIA){
-      case 0 : difficulte = 1 /*MODE FACILE -> coups au hasard uniquement*/;break;
-      case 1 : difficulte = 3 /*MODE FACILE -> 1 coup au hasard tout les trois tours*/;break;
-      case 2 : difficulte = 10 /*MODE FACILE -> 1 coup au hasard tout les dix tours*/;break;
+      case 1 : difficulte = 1 /*MODE FACILE -> coups au hasard uniquement*/;break;
+      case 2 : difficulte = 3 /*MODE INTERMEDIAIRE -> 1 coup au hasard tout les trois tours*/;break;
+      case 3 : difficulte = 10 /*MODE DIFFICILE -> 1 coup au hasard tout les dix tours*/;break;
+      case 4 : difficulte = 70 /*MODE ULTRA -> pas de coups au hasard*/;break;
       default: printf("Une erreur est survenue...");
     }
   }
@@ -120,18 +138,31 @@ int main(int argc, char * argv[]){
     // initTestPasDeBlanc(plat);
     // initTestPasDeNoir(plat);
     char * tour = NOIR;
-    while(!estFinie(plat)){
-      printf("Noir : %s / Blanc : %s",NOIR,BLANC);
-      afficherPlateau(plat);
-      if(coupPossible(plat,tour)){
-        if(choixMenu == 1){
-          saisieCoupJcJL(plat,tour,nbTours);
-        }else{
-          saisieCoupJcIA(plat,tour,nbTours,difficulte,couleurIA);
+    switch(choixMenuJ){
+      case 2:
+        while(!estFinie(plat)){
+          printf("Noir : %s / Blanc : %s",NOIR,BLANC);
+          afficherPlateau(plat);
+          if(coupPossible(plat,tour)) saisieCoupJcJD(plat,tour,nbTours);
+          tour = (tour == NOIR ? BLANC : NOIR);
+          nbTours++;
         }
-      }
-      tour = (tour == NOIR ? BLANC : NOIR);
-      nbTours++;
+        break;
+      default:
+        while(!estFinie(plat)){
+          printf("Noir : %s / Blanc : %s",NOIR,BLANC);
+          afficherPlateau(plat);
+          if(coupPossible(plat,tour)){
+            if(choixMenu == 1){
+              saisieCoupJcJL(plat,tour,nbTours);
+            }else{
+              saisieCoupJcIA(plat,tour,nbTours,difficulte,couleurIA);
+            }
+          }
+          tour = (tour == NOIR ? BLANC : NOIR);
+          nbTours++;
+        }
+        break;
     }
     afficherPlateau(plat);
     afficheScore(plat);
