625aa31a9c5b4cc563d9456653625595895de87f
Gaiall <38868450+Gaiall@users.noreply.github.com>
2019-04-16 17:50:51 +0200

    Add files via upload

--- SDL/SDLinclude.h
+++ SDL/SDLinclude.h
@@ -18,6 +18,11 @@ typedef struct s_dimension{
    int w, h;
 } t_dimension;
 
+typedef struct s_donneeSave{
+   int nbTours, difficulte, chiffreIA;
+   char * joueur;
+} t_donneeSave;
+
 SDL_Window* showWindow();
 int creerBoutons(SDL_Window * pWindow, int nbBouton, ...);
 int getRes(SDL_Window * pWindow, t_dimension resolution[], int nbRes);
@@ -37,7 +42,7 @@ int SDL_Menu3(SDL_Window * pWindow, SDL_Renderer * pRenderer);
 int SDL_AfficherMenu3(SDL_Window * pWindow, SDL_Renderer * pRenderer, SDL_Rect * tabBouton, int nbBouton);
 int SDL_Menu4(SDL_Window * pWindow, SDL_Renderer * pRenderer);
 int SDL_AfficherMenu4(SDL_Window * pWindow, SDL_Renderer * pRenderer, SDL_Rect * tabBouton, int nbBouton, t_dimension resolution[], int nbRes, int resAct);
-int SDL_MenuPause(SDL_Window * pWindow, SDL_Renderer * pRenderer);
+int SDL_MenuPause(SDL_Window * pWindow, SDL_Renderer * pRenderer, char * plat[TAILLE][TAILLE], int mode, t_donneeSave save);
 int SDL_AfficherMenuPause(SDL_Window * pWindow, SDL_Renderer * pRenderer, SDL_Rect * tabBouton, int nbBouton);
 int SDL_AfficherErreur(SDL_Renderer * pRenderer, char * error[], int nbErreur, int posXError, int posYError, int taillePolice);
 int SDL_AfficherJoueurTour(SDL_Renderer * pRenderer, int x, int y, int radius, int taillePolice, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
@@ -45,5 +50,12 @@ int SDL_AfficherBoutonMenuPause(SDL_Window * pWindow, SDL_Renderer * pRenderer, 
 /*
 *  Fonctions de jeu
 */
-int jeuJCJ(SDL_Window * pWindow, SDL_Renderer * pRenderer);
-int jeuJCIA(SDL_Window * pWindow, SDL_Renderer * pRenderer, int difficulte);
+int jeuJCJ(SDL_Window * pWindow, SDL_Renderer * pRenderer, char * plateauMat[TAILLE][TAILLE],  char * joueur, int nbTours);
+int jeuJCIA(SDL_Window * pWindow, SDL_Renderer * pRenderer, char * plateauMat[TAILLE][TAILLE], int difficulte, int nbTour, int chiffreIA);
+/*
+*  Fonctions de sauvegarde
+*/
+int saveIA(char * plat[TAILLE][TAILLE], int nbTours, int difficulte, int chiffreIA);
+int saveJCJ(char * plat[TAILLE][TAILLE], char * joueur, int nbTours);
+int loadIA(SDL_Window * pWindow, SDL_Renderer * pRenderer);
+int loadJCJ(SDL_Window * pWindow, SDL_Renderer * pRenderer);
